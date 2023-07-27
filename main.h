e5d05a5bec335c7b20dd13dfe9ce9ee907f7a104
rayen288700 <rayenchuk@gmail.com>
2023-07-27 14:22:32 +0100

    CHANGE_THE_PROTOTYP

--- main.h
+++ main.h
@@ -2,13 +2,14 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include<stddef.h>
 
-/**
- * struct pro - struct of format and function
- * @f: a function pointer
- * @X: specifier.
- */
-typedef struct pro
+	/**
+	 * struct pro - struct of format and function
+	 * @f: a function pointer
+	 * @X: specifier.
+	 */
+	typedef struct pro
 {
 	char *X;
 	int (*f)(va_list);
@@ -17,8 +18,8 @@ typedef struct pro
 int _printf(const char *, ...);
 int (*get_function(const char *C))(va_list);
 int _putchar(char c);
-int print_character(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
+int print_character(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_integer(va_list args);
 #endif
