e4ec5e4dcdf9d37ec84d9dd23022e8fa3d08ef77
rayen288700 <rayenchuk@gmail.com>
2023-07-27 11:11:58 +0100

    MY_HEADER_FILE

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/**
+ * struct pro - struct of format and function
+ * @f: a function pointer
+ * @X: specifier.
+ */
+typedef struct pro
+{
+	char *X;
+	int (*f)(va_list);
+} pro_t;
+
+int _printf(const char *, ...);
+int (*get_function(const char *C))(va_list);
+int _putchar(char c);
+int print_character(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+#endif
