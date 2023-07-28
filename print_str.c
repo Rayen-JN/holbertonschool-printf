0167acac82afcb7c89a62ecb04d5b2172d5f23cd
rayen288700 <rayenchuk@gmail.com>
2023-07-28 11:44:26 +0100

    The Function That print a string

---
+++ print_str.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_string - Prints str
+ * @args: Va_list 
+ * Return: number
+ */
+
+int print_string(va_list args)
+{
+	char *str = va_arg(args,char);
+	int count = 0;
+
+	while (*str)
+	{
+		_putchar(*str);
+		count++;
+		str++;
+	}
+	return (count);
+}
