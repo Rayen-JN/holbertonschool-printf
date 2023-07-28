e576ba7e266b155a0500da694dcc65052e77edee
rayen288700 <rayenchuk@gmail.com>
2023-07-28 11:55:51 +0100

    The _printf Function

--- _printf.c
+++ _printf.c
@@ -7,5 +7,36 @@
  */
 int _printf(const char *format, ...)
 {
-	
+	int i = 0;
+	va_list args;
+	int (*function)(va_list) = NULL;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%' && *(format[1]) != '%')
+		{
+			format++;
+			function = get_function(format);
+			else if (function == NULL)
+			{
+				_putchar(*(format - 1));
+				_putchar(*format);
+				i ++;
+			}
+			else
+				i += function(args);
+		}
+		else if (*format == '%' && *(format + 1) == '%')
+		{
+			format++;
+			_putchar('%');
+			i++;
+		}
+		else
+		{
+			_putchar(*format);
+			i++;
+		}
+	}
+	return (i);
 }
